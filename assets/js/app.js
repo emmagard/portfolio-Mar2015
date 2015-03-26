6d840846c6271a12ade12135c2ea24fcb5bbfac7
Emma Gardner <emmagardner@outlook.com>
2015-03-26 11:18:04 -0400

    final draft- still a few details to fix... contact div not touching the bottom, contact text could be better.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,48 @@
 $(document).ready(function(){
-  $('#reveal-arrow1').on('click',function(event){
+  // image carousel in other projects section
+  $('.single-item').slick();
+  // nav bar menu click and scroll animation, click and add active class
+  $('nav ul li a').on('click', function(event){
     event.preventDefault();
+
+    var fullUrl = this.href;
+    console.log(this.href);
+    var seperatedUrl = fullUrl.split('#');
+    var targ = seperatedUrl[1];
+    var targetOffsetTop = $('#'+ targ).offset().top;
+
+    var a = $('nav ul').children().children();
+    a.removeClass('active');
+
+    $(this).addClass('active');
+    console.log(this);
+
     $('html, body').animate({
-        scrollTop: $('#first-section').offset().top
-    }, 1200);
+      scrollTop: targetOffsetTop
+    }, 1500, "swing");
   });
-  $('#reveal-arrow2').on('click',function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#second-section').offset().top
-    }, 1200);
-  });
+
+  var sections = $('section');
+  var nav = $('nav');
+  var nav_height = nav.outerHeight();
+
+  $(window).on('scroll', function () {
+    // fade in nav bar opon initial window scroll
+    nav.fadeIn("slow");
+
+    var cur_pos = $(this).scrollTop();
+    // scroll and add active class when changing sections
+    sections.each(function() {
+      var top = $(this).offset().top - nav_height;
+      var bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        sections.removeClass('active');
+
+        $(this).addClass('active');
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
+});
 });
