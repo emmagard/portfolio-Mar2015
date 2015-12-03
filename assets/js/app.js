193a0789d0c84ba723ca210ea0eda0dd89b8ed8e
Emma Gardner <emmagardner@outlook.com>
2015-12-02 21:14:38 -0500

    Added a bunch of stuff, made some bug changes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,23 @@
 $(document).ready(function(){
-  // image carousel in other projects section
-  $('.single-item').slick();
-  // nav bar menu click and scroll animation, click and add active class
+
+
   $('nav ul li a').on('click', function(event){
     event.preventDefault();
 
-    var fullUrl = this.href;
-    console.log(this.href);
-    var seperatedUrl = fullUrl.split('#');
-    var targ = seperatedUrl[1];
-    var targetOffsetTop = $('#'+ targ).offset().top;
+    var fullUrl         = this.href,
+        seperatedUrl    = fullUrl.split('#'),
+        targ            = seperatedUrl[1],
+        targetOffsetTop = $('#'+ targ).offset().top;
 
-    // var a = $('nav ul').children().children();
-    // a.removeClass('active');
-
-    // $(this).addClass('active');
-    // console.log(this);
-
-    $('html, body').animate({
-      scrollTop: targetOffsetTop
-    }, 1500, "swing");
+    $('html, body').animate({ scrollTop: targetOffsetTop }, 1600, "swing");
   });
 
-  var sections = $('section');
-  var nav = $('nav');
-  var nav_height = nav.outerHeight();
+  var sections   = $('section'),
+      nav        = $('nav'),
+      nav_height = nav.outerHeight();
 
   $(window).on('scroll', function () {
-    // fade in nav bar opon initial window scroll
-    nav.fadeIn("slow");
+    nav.addClass('scrolled');
+  });
 
-    var cur_pos = $(this).scrollTop();
-    // scroll and add active class when changing sections
-    sections.each(function() {
-      var top = $(this).offset().top - nav_height;
-      var bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        nav.find('a').removeClass('active');
-        sections.removeClass('active');
-
-        $(this).addClass('active');
-        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-      }
-    });
-});
 });
